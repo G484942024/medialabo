a42eeee9e89ba46a8963a30b3d306e733a524a99
G484942024 <248494@st.takushoku-u.ac.jp>
2025-07-03 12:32:23 +0900

    Update dom2-exercise.js

--- practice-dom2/dom2-exercise.js
+++ practice-dom2/dom2-exercise.js
@@ -15,4 +15,38 @@ let gakka = [
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
+let b = document.querySelector('button#show');
+b.addEventListener('click', show);
 
+function show(){
+    //キャンパスの住所
+    let address = campus.address;
+    let p = document.createElement('p');
+    p.textContent = address;
+
+    let h2 = document.querySelector('h2#addr');
+    h2.insertAdjacentElement('afterend', p);
+
+    //学科一覧
+    let u = document.createElement('ul');
+    let l = document.createElement('li');
+
+    l.textContent = gakka[0].name;
+    u.insertAdjacentElement('beforeend', l);
+    l = document.createElement('li');
+
+    l.textContent = gakka[1].name;
+    u.insertAdjacentElement('beforeend', l);
+    l = document.createElement('li');
+
+    l.textContent = gakka[2].name;
+    u.insertAdjacentElement('beforeend', l);
+    l = document.createElement('li');
+
+    l.textContent = gakka[3].name;
+    u.insertAdjacentElement('beforeend', l);
+    l = document.createElement('li');
+
+    p = document.querySelector('h2#dept');
+    p.insertAdjacentElement('afterend', u);
+}
